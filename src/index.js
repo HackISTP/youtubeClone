7da0bc243080e43e722a6c3ec7d6e7cee9ae6bda
HackISTP <hyeminpak@gmail.com>
2023-02-27 14:09:07 +0900

    :sparkles:feat router 설정

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Videos from "./pages/Videos";
+import ErrorPage from "./pages/ErrorPage";
+import VideoDetail from "./pages/VideoDetail";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Videos /> }, //index가 최상인 경우  Videos를 보여줄것
+      { path: "videos", element: <Videos /> }, //index가 true 이거나 사용자가 명시적으로 videos라고 하는 경우에도   Videos를 보여줄것
+      { path: "/videos/:keyword", element: <Videos /> },
+      { path: "/videos/watch/:videoId", element: <VideoDetail /> },
+    ],
+  },
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
 
